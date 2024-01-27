7ee886b87ac8bc90be429e6c9f6b63667db64438
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-01-26 19:16:39 -0800

    use https only in deployed

--- Program.cs
+++ Program.cs
@@ -13,8 +13,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+else
+{
+    app.UseHttpsRedirection();
+}
 
 var summaries = new[]
 {
