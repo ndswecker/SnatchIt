605c0d3196a50f3deb22d044e2a59ceb679dd10a
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-01-30 14:20:49 -0800

    Initial incorrect post of new record

--- Controllers/RecordController.cs
+++ Controllers/RecordController.cs
@@ -45,4 +45,47 @@ public class RecordController : ControllerBase
         IEnumerable<CaptureRecord> records = _dapper.LoadDataWithParameters<CaptureRecord>(sql, sqlParameters);
         return records;
     }
+
+    [HttpPut]
+    public IActionResult EditRecord()
+    {
+        string sql = 
+            " ";
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult AddRecord(CaptureRecordDto record)
+    {
+        DynamicParameters sqlParameters = new DynamicParameters();
+
+        string sql = 
+            "INSERT INTO MicroAgeSchema.CORE ([BandNumber],[BandSize],[Scribe]," +
+            "[SpeciesCommon],[SpeciesAlpha],[SheetDate], [Station]," +
+            "[Net],[WingChord],[Sex],[AgeYear],[AgeWRP],[BodyMass]," +
+            "[Notes] ) VALUES (@BandNumber = @BandNumberParam, @BandSize = @BandSizeParam, " + 
+            "@Scribe = @ScribeParam, @SpeciesCommon = @SpeciesCommonParam, @SpeciesAlpha = @SpeciesAlphaParam, " +
+            "@SheetDate = @SheetDateParam, @Station = @StationParam, @Net = @NetParam, " +
+            "@WingChord = @WingChordParam, @Sex = @SexParam, @AgeYear = @AgeYearParam, " +
+            "@AgeWRP = @AgeWRPParam, @BodyMass = @BodyMassParam, @Notes = @NotesParam)" ;
+
+        sqlParameters.Add("@BandNumberParam", record.BandNumber, DbType.Int32);
+        sqlParameters.Add("@BandSizeParam", record.BandSize, DbType.String);
+        sqlParameters.Add("@ScribeParam", record.Scribe, DbType.String);
+        sqlParameters.Add("@SpeciesCommonParam", record.SpeciesCommon, DbType.String);
+        sqlParameters.Add("@SpeciesAlphaParam", record.SpeciesAlpha, DbType.String);
+        sqlParameters.Add("@SheetDateParam", record.SheetDate, DbType.DateTime);
+        sqlParameters.Add("@StationParam", record.Station, DbType.String);
+        sqlParameters.Add("@NetParam", record.Net, DbType.String);
+        sqlParameters.Add("@WingChordParam", record.WingChord, DbType.Int32);
+        sqlParameters.Add("@SexParam", record.Sex, DbType.StringFixedLength);
+        sqlParameters.Add("@AgeYearParam", record.AgeYear, DbType.String);
+        sqlParameters.Add("@AgeWRPParam", record.AgeWRP, DbType.String);
+        sqlParameters.Add("@BodyMassParam", record.BodyMass, DbType.Decimal);
+        sqlParameters.Add("@NotesParam", record.Notes, DbType.String);
+
+
+        _dapper.ExecuteSqlWithParameters(sql, sqlParameters);
+        return Ok();
+    }
 }
---
+++ Models/CaptureRecordDto.cs
@@ -0,0 +1,21 @@
+namespace SnatchItAPI.Models
+{
+    public partial class CaptureRecordDto
+    {
+        public int BandNumber { get; set; }
+        public string BandSize { get; set; } = "";
+        public string Scribe { get; set; } = "";
+        public string SpeciesCommon { get; set; } = "";
+        public string SpeciesAlpha { get; set; } = "";
+        public DateTime SheetDate { get; set; }
+        public string Station { get; set; } = "";
+        public string Net { get; set; } = "";
+        public int WingChord { get; set; }
+        public char Sex { get; set; } = 'U';
+        public string AgeYear { get; set; } = "";
+        public string AgeWRP { get; set; } = "";
+        public decimal BodyMass { get; set; }
+        public string Notes { get; set; } = "";
+    }
+
+}
