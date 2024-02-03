0d4c021c5ce0eb1545d4207cce96121821450eb3
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-02-03 10:07:34 -0800

    Bander registration checks
    Does not actually update the db yet, but correctly checks if bander already exists, and if password and confirm matches

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,6 +1,8 @@
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using SnatchItAPI.Data;
 using SnatchItAPI.Models;
+using System.Data;
 
 namespace SnatchItAPI.Controllers
 {
@@ -19,9 +21,19 @@ namespace SnatchItAPI.Controllers
         {
             if (string.Equals(banderForRegistration.Password, banderForRegistration.PasswordConfirm) )
             {
-                return Ok("You have registered");
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@EmailParam", banderForRegistration.Email, DbType.String);
+                string sqlCheckBanderExists = "SELECT Email FROM MicroAgeSchema.Auth WHERE Email = @EmailParam ";
+
+                IEnumerable<string> existingBanders = _dapper.LoadDataWithParameters<string>(sqlCheckBanderExists, parameters);
+
+                if (existingBanders.Count() == 0)
+                {
+                    return Ok($"Thank you {banderForRegistration.Email}! You are now registered :)");
+                }
+                return StatusCode(409, "Bander with that email already exists.");
             }
-            return NotFound("Failure to register new bander");
+            return StatusCode(400, "Failure to register new bander, passwords do not match");
         }
 
         [HttpPost("Login")]
