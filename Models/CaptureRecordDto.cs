8664f8f3181c3ac3b0eee1e04fb6e3a20dfc1ff5
Nathanael Swecker <nathanael.swecker@gmail.com>
2024-02-12 18:49:40 -0800

    Adding additional fields to CaptureRecordDto

--- Models/CaptureRecordDto.cs
+++ Models/CaptureRecordDto.cs
@@ -6,17 +6,28 @@ namespace SnatchItAPI.Models
         public string BandSize { get; set; } = "";
         public string Bander { get; set; } = "";
         public string Scribe { get; set; } = "";
+        public string Code { get; set; } = "";
         public string SpeciesCommon { get; set; } = "";
         public string SpeciesAlpha { get; set; } = "";
         public DateTime SheetDate { get; set; }
         public string Station { get; set; } = "";
         public string Net { get; set; } = "";
+        public string CloacaShape { get; set; } = "";
+        public int CPScore { get; set; }
+        public int BPScore { get; set; }
+        public int FatScore { get; set; }
+        public int BodyMoltScore { get; set; } 
+        public string FFMolt { get; set;} = "";
+        public int FFWearScore { get; set; }
+        public int JuvBodyPlumScore { get; set;}
+        public int SkullScore { get; set; }
         public int WingChord { get; set; }
         public char Sex { get; set; } = 'U';
         public string AgeYear { get; set; } = "";
         public string AgeWRP { get; set; } = "";
         public decimal BodyMass { get; set; }
         public string Notes { get; set; } = "";
+        public int ReleaseCondition { get; set;}
     }
 
 }
